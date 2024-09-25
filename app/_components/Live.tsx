553e7d1e5f0c315d6d439c93367521922c31897a
Zeyad Lotfy <zezoramy380@gmail.com>
2024-09-25 14:15:48 +0300

    first commit

--- app/_components/Live.tsx
+++ app/_components/Live.tsx
@@ -1,10 +1,10 @@
-import { liveData } from "../_types/app";
+import { LiveData } from "../_types/app";
 import { getLive } from "../_utils/api";
 import LiveCard from "./LiveCard";
 import MainHeading from "./MainHeading";
 
 const Live = async () => {
-  const liveData: liveData = await getLive();
+  const liveData: LiveData = await getLive();
 
   return (
     <section className="p-16">
